7c26f72b16ca4cd105121fbcad9db6a72924f5a2
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-20 10:50:09 +0800

    day 1

---
+++ day 1/03_variables.py
@@ -0,0 +1,4 @@
+name = "Andreaville"
+age = 22 
+print(f"My name is {name}")
+print(f"My age is {age}")
---
+++ day 1/05_datatypes.py
@@ -0,0 +1,9 @@
+integer_num = 12
+float_num = 12.12
+string_text = "This is Python"
+boolean_text = True
+
+print(integer_num)
+print(float_num)
+print(string_text)
+print(boolean_text)
