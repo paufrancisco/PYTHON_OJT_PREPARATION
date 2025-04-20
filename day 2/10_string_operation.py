58afa21f450b577415f2409fc3f35af2c9b886e0
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-20 11:09:46 +0800

    add new

---
+++ day 2/10_string_operation.py
@@ -0,0 +1,13 @@
+text = "Hello World"
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.replace("World", "Python"))
+print(f"Length of text: {len(text)}")
+
+my_name = "Paulo"
+print(my_name.upper())
+print(my_name.lower())
+print(my_name.capitalize()) #sentence case 
+print(my_name.replace("Paulo","Ken"))
+print(f"Length of text: {len(my_name)}")
