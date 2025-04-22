1719af84497dade1f55f1b7b08c7ba4168196432
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-22 11:06:39 +0800

    add new

---
+++ day 4/20_json_handling.py
@@ -0,0 +1,17 @@
+import json
+
+data = {
+    
+    "name" : "Paulo",
+     "age" : 29,
+    "city" : "Dasma Cavite"
+}
+
+# convert to json string
+json_string = json.dumps(data)
+print("JSON string:", json_string)
+
+# convert back to python dict
+
+parsed_data = json.loads(json_string)
+print("Parsed data: ", parsed_data)
