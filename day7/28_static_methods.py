d9eec4ac9161a17fa9c753239fb6adc03df06afb
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-26 17:29:48 +0800

    add new

---
+++ day7/28_static_methods.py
@@ -0,0 +1,11 @@
+class MathTools:
+    
+    def add (a, b):
+        return a + b
+    
+    def mul(a , b):
+        return a*b
+    
+print(f"Addition: {MathTools.add(3, 4)}")
+print(f"Multiplication: {MathTools.mul(3, 4)}")
+
