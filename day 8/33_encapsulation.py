d73266bdbc3983ae65ec55f79f166a73c8a28f9c
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-29 08:35:57 +0800

    add new

---
+++ day 8/33_encapsulation.py
@@ -0,0 +1,25 @@
+class BankAccount:
+    def __init__(self,balance):
+        self.__balance = balance
+        
+    def deposit(self, amount):
+        self.__balance += amount
+        
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Not enough balance")
+            
+    def get_balance(self):
+        return self.__balance
+    
+    
+account = BankAccount(1000)
+
+print(f"Old Balance: {account.get_balance()}")
+account.deposit(1000)
+account.withdraw(1000)
+account.withdraw(1000)
+account.withdraw(200)
+print(f"New Balance: {account.get_balance()}")
