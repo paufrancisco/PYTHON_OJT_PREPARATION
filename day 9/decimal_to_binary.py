4219e6d277a22094127140998ed4191a6d2bd7b4
paufrancisco <francisco.paulo.pf@gmail.com>
2025-04-30 17:47:50 +0800

    modify

--- day 9/decimal_to_binary.py
+++ day 9/decimal_to_binary.py
@@ -4,13 +4,13 @@ def decimal_to_binary(decimal_num):
     
     binary_str = ''
     while decimal_num > 0:
-        remainder = decimal_num % 2      # Get remainder (0 or 1)
-        binary_str = str(remainder) + binary_str  # Append remainder to the front
-        decimal_num = decimal_num // 2   # Update decimal number with quotient
+        remainder = decimal_num % 2     
+        binary_str = str(remainder) + binary_str  # store the string array
+        decimal_num = decimal_num // 2  
     
     return binary_str
 
-# Example usage
+
 decimal_input = int(input("Enter a decimal number: "))
 binary_output = decimal_to_binary(decimal_input)
 print(f"Binary equivalent: {binary_output}")
