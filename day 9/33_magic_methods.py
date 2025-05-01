9630a5dca5769f26b2dfd1e141562d6f60a3ea5f
paufrancisco <francisco.paulo.pf@gmail.com>
2025-05-01 20:07:11 +0800

    add new

---
+++ day 9/33_magic_methods.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+    def __str__(self):
+        return f"Book: {self.title}"
+
+    def __len__(self):
+        return len(self.title)
+
+# Usage
+book = Book("Python 101")
+print(book)         # Output: Book: Python 101
+print(len(book))    # Output: 10
