c90bf7e457c6e75035521a02d80055bfba4275a4
paufrancisco <francisco.paulo.pf@gmail.com>
2025-05-02 19:02:22 +0800

    modify day 30

--- day 9/30_abstract_class.py
+++ day 9/30_abstract_class.py
@@ -1,21 +1,49 @@
 from abc import ABC, abstractmethod
 
-# Abstract base class
 class Animal(ABC):
     @abstractmethod
     def make_sound(self):
         pass
 
-# Subclass must implement make_sound
-class Dog(Animal):
+class Rabbit(Animal):
     def make_sound(self):
-        print("Woof!")
-
+        print("Im rabbit")
+        
 class Cat(Animal):
     def make_sound(self):
-        print("Meow!")
+        print("Im cat")
 
-# Usage
-animals = [Dog(), Cat()]
+
+animals = [Rabbit(), Cat()]
+
 for animal in animals:
-    animal.make_sound()  # Woof! Meow!
+    animal.make_sound()
+
+
+
+
+
+
+
+
+# from abc import ABC, abstractmethod
+
+# # Abstract base class
+# class Animal(ABC):
+#     @abstractmethod
+#     def make_sound(self):
+#         pass
+
+# # Subclass must implement make_sound
+# class Dog(Animal):
+#     def make_sound(self):
+#         print("Woof!")
+
+# class Cat(Animal):
+#     def make_sound(self):
+#         print("Meow!")
+
+# # Usage
+# animals = [Dog(), Cat()]
+# for animal in animals:
+#     animal.make_sound()  # Woof! Meow!
