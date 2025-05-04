d4e45f41a06931b029b9bace36ba90290d867096
paufrancisco <francisco.paulo.pf@gmail.com>
2025-05-04 18:34:48 +0800

    add new

---
+++ day 10/34_custom_package/34_use_custom_package.py
@@ -0,0 +1,6 @@
+# Using the custom package
+
+from custom_package import geometry  # ✅ works when run directly
+
+print("Square area:", geometry.area_square(5))          # Output: 25
+print("Rectangle area:", geometry.area_rect(5, 4)) # Output: 20
