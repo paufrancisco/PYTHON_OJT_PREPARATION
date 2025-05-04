340b82a2d7d7f97a9881f6f03be2118ae0e11444
paufrancisco <francisco.paulo.pf@gmail.com>
2025-05-04 18:28:20 +0800

    add new

---
+++ day 10/34_custom_package/geometry.py
@@ -0,0 +1,9 @@
+def area_square(side): 
+     return side**2
+ 
+def area_rect(length,width):
+    return length*width
+
+
+print(f"The area of square: {area_square(5)}")
+print(f"The area of square: {area_rect(5,5)}")
